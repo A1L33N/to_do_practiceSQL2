f0cec6c211b2b2f29bc7f25cfc4448d82d2ef378
Michael Bunker & Aileen Cacayorin <student@epicodus.com>
2015-08-18 11:27:59 -0700

    List page will add and view lists

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
 require './lib/to_do.rb'
 require 'sinatra/reloader'
+require './lib/list.rb'
 require 'sinatra'
 also_reload '../lib/**/*.rb'
 require 'pg'
@@ -14,9 +15,14 @@ get('/add_lists') do
   erb(:add_list)
 end
 
-post('/view_list') do
+get('/view_lists') do
+  erb(:view_lists)
+end
+
+
+post('/view_lists') do
   name = params.fetch('list_name')
-  list = List.new({:name => name, id => nil})
-  list.save()
-  erb(:view_list)
+  @list = List.new({:name => name, :id => nil})
+  @list.save()
+  erb(:view_lists)
 end
